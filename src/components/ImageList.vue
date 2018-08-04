9961551b2c837d5c8b262ba8e1778fd2364d7587
Imran Askem <imran.askem@gmail.com>
2018-08-04 13:08:54 +0100

    Fixed 403 error if user is not logged in

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,8 +1,14 @@
 <template>
     <div>
-        <img v-for="image in allImages"
-        :key="image.id"
-        :src="image.link" class="ui small image" />
+        <div v-if="isLoggedIn" class="image-container">
+            <img
+                v-for="image in allImages"
+                :key="image.id"
+                :src="image.link"
+            />
+        </div>
+
+        <h2 v-else>Please login to see your images</h2>
     </div>
 </template>
 
@@ -12,14 +18,27 @@ import { mapActions, mapGetters } from 'vuex';
 export default {
     name: 'ImageList',
     computed: {
-        ...mapGetters(['allImages'])
+        ...mapGetters(['allImages', 'isLoggedIn'])
     },
     methods: {
         ...mapActions(['fetchImages'])
     },
     created() {
-        this.fetchImages();
+        if (this.isLoggedIn) {
+            this.fetchImages();
+        }
     }
 }
 </script>
 
+<style scoped>
+    .image-container{
+        column-count: 3;
+        column-gap: 0;
+    }
+
+    img {
+        max-width: 100%;
+        padding: 5px;
+    }
+</style>
